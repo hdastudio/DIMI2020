25165f0efb94c0a136aac2517b6e16a68a8c6f6f
niki <v-niki>
2021-01-18 09:47:13 +0400

    heroku2

--- src/main/java/com/example/Model/BookController.java
+++ src/main/java/com/example/Model/BookController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,10 +15,10 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @GetMapping("/")
+    @GetMapping("/all")
     public String index(ModelMap model) {
         model.addAttribute("books", bookService.findAll());
-        return "index";
+        return "index1";
     }
 
     @PostMapping("/add")
